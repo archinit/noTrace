338fd51b720c649cd64e7f57a99bd32700562abf
archinit <archit.7537@gmail.com>
2026-01-19 18:51:34 +0530

    landing page edited

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <div>
-  	Hi from WSL terminal
-  </div>
+  return <main className="flex min-h-screen flex-col items-center justify-center p-4">
+      <div className="w-full max-w-md space-y-8">
+        <div className="vorder border-zinc-800 bg-zinc-900/50 p-6 backdrop-blur-md">
+          <div className="space-y-5">
+            <div>
+              <div className="space-y-2">
+                create room
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </main>
 }
