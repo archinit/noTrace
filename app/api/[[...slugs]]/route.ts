142d907f9e756dbe7a3d8507a959777ef6ee0a1a
archinit <archit.7537@gmail.com>
2026-01-26 17:52:57 +0530

    updated: GET endpoint for messages history

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -66,6 +66,21 @@ const messages = new Elysia({ prefix:
             text: z.string().max(1000),
         })
     })
+    .get("/", async ({ auth }) => {
+        const messages = await redis.lrange<Message>(`messages:${auth.roomId}`, 0, -1)
+
+        return {
+            messages: messages.map((m) => ({
+                ...m,
+                token: m.token === auth.token ? auth.token : undefined,
+
+            })),
+        }
+    },
+    {
+        query: z.object({ roomId: z.string()})
+    }
+)
 
 const app = new Elysia({ prefix: '/api' }).use(rooms).use(messages);
 export const GET = app.fetch 
